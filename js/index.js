ff74fe3b7d074150971869a7b2473837047313a1
Marat <thisismarat123@gmail.com>
2023-05-10 01:32:12 +0500

    5/9/2023 evening

--- js/index.js
+++ js/index.js
@@ -1,54 +1,67 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-     // Поочерёдное появление блоков
+    // Поочерёдное появление блоков
     const blocks = document.querySelectorAll('.animated__block');
     const sectionOne = document.querySelector('.section__one');
     const sectionTwo = document.querySelector('.section__two');
 
-    window.addEventListener('scroll', function(){
-        let elementStart = sectionOne.offsetHeight * (1/2);
+    window.addEventListener('scroll', function () {
+        let elementStart = sectionOne.offsetHeight * (1 / 2);
         let scrollTop = window.scrollY;
-        if(scrollTop >= elementStart) {
+        if (scrollTop >= elementStart) {
             blocks[0].classList.add('animated');
         }
     });
-    window.addEventListener('scroll', function(){
-        let elementStart = sectionOne.offsetHeight * (15/20);
+    window.addEventListener('scroll', function () {
+        let elementStart = sectionOne.offsetHeight * (15 / 20);
         let scrollTop = window.scrollY;
-        if(scrollTop >= elementStart) {
+        if (scrollTop >= elementStart) {
             blocks[1].classList.add('animated');
         }
     });
-    window.addEventListener('scroll', function(){
-        let elementStart = sectionOne.offsetHeight + sectionTwo.offsetHeight * (1/5);
+    window.addEventListener('scroll', function () {
+        let elementStart = sectionOne.offsetHeight + sectionTwo.offsetHeight * (1 / 5);
         let scrollTop = window.scrollY;
-        if(scrollTop >= elementStart){
+        if (scrollTop >= elementStart) {
             blocks[2].classList.add('animated');
+        }
+    });
+
+    // НАШИ УЧИТЕЛЯ
+    const sloganTeachers = document.querySelector('.section__three__slogan');
+    window.addEventListener('scroll', function () {
+        let elementStart = sectionOne.offsetHeight + sectionTwo.offsetHeight * (10/15);
+        let scrollTop = window.scrollY;
+        console.log(scrollTop);
+        console.log(elementStart);
+        if (scrollTop >= elementStart) {
+            sloganTeachers.classList.add('animated');
         }
     });
 
     // Появление и исчезание header
     const header = document.querySelector('.header');
     let lastScroll = 0;
-
     const containsHide = header.classList.contains('hide');
 
-    document.addEventListener('scroll', function(){
-        if (window.scrollY > lastScroll && !containsHide && window.scrollY > 250){
+    document.addEventListener('scroll', function () {
+        if (window.scrollY > lastScroll && !containsHide && window.scrollY > 250) {
             header.classList.add('header__hide');
-        } else if (window.scrollY < lastScroll){
+        } else if (window.scrollY < lastScroll) {
             header.classList.remove('header__hide');
         }
 
 
         lastScroll = window.scrollY;
     });
-    
-    
-    
-    
+
+
+
+
 });
 
-$(document).ready(function(){
+// слайдер J-Query
+
+$(document).ready(function () {
     $('.slider').slick();
 });
